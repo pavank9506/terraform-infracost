f48454fbaf84b9d58704f932b5a554ce62b01937
Your Name <pavan.kota439@email.com>
2026-02-03 12:35:13 +0530

    commiting

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,129 +23,133 @@ resource "aws_internet_gateway" "main" {
   )
 }
 
-resource "aws_subnet" "private" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidrs
-
-  tags = merge(
-    var.common_tags,
-    var.private_subnet_tags,
-    {
-        Name = "${local.resource_name}-private"
-    }
-  )
-}
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidrs
-
-  tags = merge(
-    var.common_tags,
-    var.public_subnet_tags,
-    {
-        Name = "${local.resource_name}-public"
-    }
-  )
-}
-
-resource "aws_subnet" "database" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.database_subnet_cidrs
-
-  tags = merge(
-    var.common_tags,
-    var.database_subnet_tags,
-    {
-        Name = "${local.resource_name}-database"
-    }
-  )
-}
-
 resource "aws_eip" "nat" {
   domain   = "vpc"
 }
 
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public.id
+# resource "aws_subnet" "private" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = var.private_subnet_cidrs
 
-  tags = merge(
-    var.common_tags,
-    var.nat_gateway_tags,
-    {
-        Name = local.resource_name
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.private_subnet_tags,
+#     {
+#         Name = "${local.resource_name}-private"
+#     }
+#   )
+# }
 
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_subnet" "public" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = var.public_subnet_cidrs
 
-  tags = merge(
-    var.common_tags,
-    var.public_route_table_tags,
-    {
-      Name = "${local.resource_name}-public" #dev-public
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.public_subnet_tags,
+#     {
+#         Name = "${local.resource_name}-public"
+#     }
+#   )
+# }
 
-# private route table
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_subnet" "database" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = var.database_subnet_cidrs
 
-  tags = merge(
-    var.common_tags,
-    var.private_route_table_tags,
-    {
-      Name = "${local.resource_name}-private" #dev-private
-    }
-  )
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.database_subnet_tags,
+#     {
+#         Name = "${local.resource_name}-database"
+#     }
+#   )
+# }
 
-# database route table
-resource "aws_route_table" "database" {
-  vpc_id = aws_vpc.main.id
+# resource "aws_eip" "nat" {
+#   domain   = "vpc"
+# }
 
-  tags = merge(
-    var.common_tags,
-    var.database_route_table_tags,
-    {
-      Name = "${local.resource_name}-database" #dev-database
-    }
-  )
-}
+# resource "aws_nat_gateway" "main" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = aws_subnet.public.id
 
-resource "aws_route" "public" {
-  route_table_id            = aws_route_table.public.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.nat_gateway_tags,
+#     {
+#         Name = local.resource_name
+#     }
+#   )
+# }
 
-resource "aws_route" "private_nat" {
-  route_table_id            = aws_route_table.private.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.main.id
-}
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-resource "aws_route" "database_nat" {
-  route_table_id            = aws_route_table.database.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.main.id
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.public_route_table_tags,
+#     {
+#       Name = "${local.resource_name}-public" #dev-public
+#     }
+#   )
+# }
 
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
+# # private route table
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
-}
+#   tags = merge(
+#     var.common_tags,
+#     var.private_route_table_tags,
+#     {
+#       Name = "${local.resource_name}-private" #dev-private
+#     }
+#   )
+# }
 
-resource "aws_route_table_association" "database" {
-  subnet_id      = aws_subnet.database.id
-  route_table_id = aws_route_table.database.id
-}
+# # database route table
+# resource "aws_route_table" "database" {
+#   vpc_id = aws_vpc.main.id
+
+#   tags = merge(
+#     var.common_tags,
+#     var.database_route_table_tags,
+#     {
+#       Name = "${local.resource_name}-database" #dev-database
+#     }
+#   )
+# }
+
+# resource "aws_route" "public" {
+#   route_table_id            = aws_route_table.public.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   gateway_id = aws_internet_gateway.main.id
+# }
+
+# resource "aws_route" "private_nat" {
+#   route_table_id            = aws_route_table.private.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.main.id
+# }
+
+# resource "aws_route" "database_nat" {
+#   route_table_id            = aws_route_table.database.id
+#   destination_cidr_block    = "0.0.0.0/0"
+#   nat_gateway_id = aws_nat_gateway.main.id
+# }
+
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.public.id
+#   route_table_id = aws_route_table.public.id
+# }
+
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.private.id
+#   route_table_id = aws_route_table.private.id
+# }
+
+# resource "aws_route_table_association" "database" {
+#   subnet_id      = aws_subnet.database.id
+#   route_table_id = aws_route_table.database.id
+# }
